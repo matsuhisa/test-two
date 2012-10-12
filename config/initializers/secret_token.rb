317907bd68a67a644505c509298b7baba7f6b35b
matsuhisa <matsuhisa@localhost.localdomain>
2012-10-12 15:20:10 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twed::Application.config.secret_token = '6512010e05f398521ffeffc22ec4d69eefad138d7c23244304ee34b1602cf231cc5b38b7b689306e0b738b4a28ccd6f8b4d8c9d00240e0dbba2ad559dd701666'
